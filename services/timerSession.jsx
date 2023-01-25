3621a0a28d4d919075da57fb68786863f97ee3e3
ReA974 <erwan.com46@gmail.com>
2023-01-25 16:59:29 +0100

    add timer on session only work with button once the session can t start for the moment

--- services/timerSession.jsx
+++ services/timerSession.jsx
@@ -1,37 +1,41 @@
-import { useState } from 'react';
-import useSessionStorage from './useSessionStorage';
-
-const [sessionTimeStart, setSessionTimeStart] = useSessionStorage('sessionTimeStart', 0);
-const [sessionTimeEnd, setSessionTimeEnd] = useSessionStorage('sessionTimeEnd', 0);
-const [sessionTime, setSessionTime] = useState(0);
-
-// start the timer
-function startSessionTimer(session) {
-  const sessionTimeStartTemp = Date.now();
-  if (session !== null) {
-    setSessionTimeStart(sessionTimeStartTemp);
-  }
+import {
+  doc, setDoc, Timestamp, getDocs, collection,
+} from 'firebase/firestore';
+import { db } from './firebaseConfig';
+// Querry to store data in a group and find that by phone number at the start of the session
+async function startTimer(user) {
+  const phoneNumber = `0${user.phoneNumber.substring(3)}`;
+  const start = Timestamp.fromDate(new Date());
+  await setDoc(doc(db, 'GROUP', phoneNumber), {
+    startSession: start,
+  });
 }
 
-// calculate the time
-
-function calculateSessionTime(session) {
-  const sessionTimeTemp = sessionTimeEnd - sessionTimeStart;
-  if (session !== null) {
-    setSessionTime(sessionTimeTemp);
-  }
+// Store data at the end of the session and calculate the time
+async function stopTimer(user) {
+  const phoneNumber = `0${user.phoneNumber.substring(3)}`;
+  const stop = Timestamp.fromDate(new Date());
+  await setDoc(doc(db, 'GROUP', phoneNumber), {
+    endSession: stop,
+  }, { merge: true });
 }
 
-// stop the timer
-
-function stopSessionTimer(session) {
-  const sessionTimeEndTemp = Date.now();
-  if (session !== null) {
-    setSessionTimeEnd(sessionTimeEndTemp);
-  }
-  calculateSessionTime(session);
+// Get the time of the last session
+async function getDeltaTime(user) {
+  let Delta;
+  const phoneNumber = `0${user.phoneNumber.substring(3)}`;
+  const querySnapshot = await getDocs(collection(db, 'GROUP'));
+  querySnapshot.forEach((doc1) => {
+    if (doc1.id === phoneNumber) {
+      const start = doc1.data().startSession;
+      const end = doc1.data().endSession;
+      const delta = end - start;
+      Delta = delta / 60;
+    }
+  });
+  await setDoc(doc(db, 'GROUP', phoneNumber), {
+    deltaSession: Delta,
+  }, { merge: true });
 }
 
-export {
-  startSessionTimer, stopSessionTimer, calculateSessionTime, sessionTime,
-};
+export { startTimer, stopTimer, getDeltaTime };
